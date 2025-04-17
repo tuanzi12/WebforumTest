75fe3bd7cff243b22ed360e987f747fd29809e52
tuanzi12 <1967179366@qq.com>
2025-04-17 13:50:28 +0800

    网页论坛测试项目

--- tests/ForumPersonal.py
+++ tests/ForumPersonal.py
@@ -73,7 +73,7 @@ class ForumPersonal:
         time.sleep(0.5)
         wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#settings_submit_password"))).click()# 点击修改密码按钮
         #重新登录验证
-        self.driver.find_element(By.CSS_SELECTOR, "#username").send_keys("TzTest1")
+        self.driver.find_element(By.CSS_SELECTOR, "#username").send_keys("tuanzi_test4")
         self.driver.find_element(By.CSS_SELECTOR, "#password").send_keys("newpsw1")
         self.driver.find_element(By.CSS_SELECTOR, "#submit").click()
         self.driver.find_element(By.CSS_SELECTOR, "#index_nav_nickname")
--- tests/RunTest.py
+++ tests/RunTest.py
@@ -6,13 +6,12 @@ from common.Utils import forumDriver
 
 if __name__ == "__main__":
         # 注册失败/注册成功
-    # ForumRegister.ForumRegister().RegisterFailTest()
-    # ForumRegister.ForumRegister().RegisterSucTest()
-    #     # 登录失败/登录成功
+    ForumRegister.ForumRegister().RegisterFailTest()
+    ForumRegister.ForumRegister().RegisterSucTest()
+         # 登录失败/登录成功
     ForumLogin.ForumLogin().LoginFailTest()
     ForumLogin.ForumLogin().LoginSucTest()
-        # 修改头像
-    # ForumPersonal.ForumPersonal().Change_ProfilePhoto()
+
         # 修改个人信息
     ForumPersonal.ForumPersonal(). PersonalProfileChange()
         # 个人页面修改密码成功/失败
